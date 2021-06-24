3927fbe543b74b130fa30d4e10b2124ca982c059
Jinhan Wu <jinhanwu@JinhandeMacBook-Air.local>
2021-06-24 11:45:07 -0400

    my first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+        <h1>p</h1>
+        <p>
+          urmum
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=xfr64zoBTAQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn about me
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

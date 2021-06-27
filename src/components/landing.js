15e41f35b5acbea7d386e69b5bf822902e1e275e
Jinhan Wu <jinhanwu@JinhandeMacBook-Air.local>
2021-06-27 15:34:03 -0400

    update landing page

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 
+import cat1 from '../assets/cat1.jpeg'
+import cat2 from '../assets/cat2.jpeg'
+import cat3 from '../assets/cat3.png'
+
 /**
  * basic component for copy. This is a basic react component
  */
@@ -10,7 +14,23 @@ class Landing extends Component {
 
     render() {
         return (
-            <h1>welcome</h1>
+
+            <div className="page">
+                <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src={cat1} className="aspect-fill" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src={cat2} className="aspect-fill" alt="..." />
+                        </div>
+                        <div className="carousel-item">
+                            <img src={cat3} className="aspect-fill" alt="..." />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         )
     }
 }

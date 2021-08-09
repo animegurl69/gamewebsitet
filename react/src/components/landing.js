74da092227a2ef28f770f46bb23c3eff15f48c36
Jinhan Wu <jinhanwu@JinhandeMacBook-Air.local>
2021-08-09 14:33:27 -0400

    carousel

--- react/src/components/landing.js
+++ react/src/components/landing.js
@@ -16,52 +16,52 @@ class Landing extends Component {
 
     render() {
         return (
-            <div className = "home-page">
-                
-                <div className= "whiteblock">
+            <div className="home-page">
+
+                <div className="whiteblock">
                 </div>
-            <div className="container mt-5 landing-container">
-                
-                <div id="carouselWithControls" className="carousel slide" data-bs-ride="carousel">
-                    <div className="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <div className="container mt-5 landing-container">
+
+                    <div id="carouselWithControls" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselWithControls" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselWithControls" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselWithControls" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src={art2} className="active aspect-fill-center" alt="..." />
+                                <div className="carousel-caption d-none d-md-block">
+                                    <h4 className="pink-text">Art</h4>
+                                    <p className="pink-text"> cool art for our game</p>
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={cat2} className="aspect-fill" alt="..." />
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h4 className="pink-text">Cat 2</h4>
+                                    <p className="pink-text">Some representative placeholder content for the first slide.</p>
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={cat3} className="aspect-fill" alt="..." />
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h4 className="pink-text">Cat 3</h4>
+                                    <p className="pink-text">Some representative placeholder content for the first slide.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselWithControls" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#carouselWithControls" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
                     </div>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <img src={art2} className="active aspect-fill-center" alt="..." />
-                            <div className="carousel-caption d-none d-md-block">
-                                <h4 className= "pink-text">Art</h4>
-                                <p className= "pink-text"> cool art for our game</p>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src={cat2} className="aspect-fill" alt="..." />
-                            <div class="carousel-caption d-none d-md-block">
-                                <h4 className= "pink-text">Cat 2</h4>
-                                <p className= "pink-text">Some representative placeholder content for the first slide.</p>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src={cat3} className="aspect-fill" alt="..." />
-                            <div class="carousel-caption d-none d-md-block">
-                                <h4 className= "pink-text">Cat 3</h4>
-                                <p className= "pink-text">Some representative placeholder content for the first slide.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselWithControls" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselWithControls" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
                 </div>
-            </div>
-            <Footer/>
+                <Footer />
             </div>
         )
     }
